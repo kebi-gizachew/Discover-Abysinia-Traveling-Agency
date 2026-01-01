87a62b7977ac81cd4d7fe1ff1104122a5ac930ff
hawi088 <hawichalaf8@gmail.com>
2026-01-02 00:15:56 +1300

    Debug Route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Destination from './pages/Destination';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Login from './pages/login';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/destination" element={<Destination />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
