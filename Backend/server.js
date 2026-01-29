6951f5c136786b619897e6648b4c55765e9f1fb0
hawi088 <hawi.ugr-4097-16@aau.edu.et>
2026-01-30 09:20:28 +1300

    improved booking css

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,8 @@ app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true
 }))
-app.use(express.json())
+app.use(express.json({ limit: "5mb" })); 
+
 app.use(cookieParser())
 
 connectDB();
